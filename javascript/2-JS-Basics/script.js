3cf1db281c2edf94f49fd6b628b3c824bf336ebb
Anand Rajaram <anandrajaram21@gmail.com>
2020-03-15 19:06:00 +0530

    Finished till lecture 09

---
+++ javascript/2-JS-Basics/script.js
@@ -0,0 +1,53 @@
+/************************
+*Variables and data types
+*/
+
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+// var if = 23; Will throw an error
+*/
+
+
+/********************************
+* Variable mutation and type coercion
+*/
+
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName)
+*/
